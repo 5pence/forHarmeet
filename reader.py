aa10faa6bc073691461a48c106147f7048817dfc
spencer barriball <sbarriball@me.com>
2024-12-18 15:45:50 +0000

    add comments to reader.py

--- reader.py
+++ reader.py
@@ -1,22 +1,27 @@
 import ast
 
+# open a file reader in reading mode
 with open('harmeetsNumber.txt', 'r') as f:
+    # remove spaces etc
     raw_data = f.read().strip()
 
+# make raw_data like a 2d list
 raw_data = raw_data.replace('{', '[').replace('}', ']')
 
+# evaluate the string
 data_list = ast.literal_eval(raw_data)
 
+# create a empty dict
 result = {}
 
+# loop and add to dict
 for name, value in data_list:
     if name not in result:
         result[name] = []
     result[name].append(value)
 
-print(result)
-
+# now loop throug and find average
+# and print them for each item
 for key, values in result.items():
     avg = sum(values) / len(values)
     print(f"{key} has an average score of {avg}")
-
